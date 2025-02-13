936c4d9328298031b63fbe966ff55f3367af7ba9
Rachit Gaunkar <rachit.gaunkar@zapcg.com>
2025-02-13 14:56:24 +0530

    updated my database 2

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,25 +1,14 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = process.env.DATABASE_URL
-  ? new Sequelize(process.env.DATABASE_URL, {
-      dialect: "postgres",
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false, // Required for Render
-        },
-      },
-    })
-  : new Sequelize(
-      process.env.PG_DATABASE, // Local Database name
-      process.env.PG_USER, // Local Database user
-      process.env.PG_PASSWORD, // Local User password
-      {
-        host: process.env.PG_HOST, // Local Database host
-        port: process.env.PG_PORT, // Local Database port
-        dialect: "postgres",
-      }
-    );
+const db = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Required for Render's SSL
+    },
+  },
+});
 
-module.exports = sequelize;
+module.exports = db;
